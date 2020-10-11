e215af57317220fbe671675869f337d349229357
Ytr00m <dan.ro.sousa@gmail.com>
2020-10-11 13:09:22 -0300

    SelectionSort em Python

---
+++ Codigos/SelectionSort.py
@@ -0,0 +1,15 @@
+def SelectionSort(lista):
+    
+    for i in range(len(lista)-1):
+        menor = lista[i]
+        posJ = i
+
+        for j in range(i+1, len(lista)):
+
+            if menor > lista[j]:
+                menor = lista[j]
+                posJ = j
+
+        lista[i], lista[posJ] = lista[posJ], lista[i]
+        
+    return lista
